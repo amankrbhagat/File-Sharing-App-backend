d612e094515c06e45fb5ba2520f81ac18311278b
Aman Kumar Bhagat <bhagatamankumar1999@gmail.com>
2023-10-13 11:13:15 +0530

    file-sharing-app

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const DBConnection = async () => {
     
-    const MONGO_URI = `mongodb://bhagatamankumar1999:Amankumar@ac-kxcl5ym-shard-00-00.s7g6for.mongodb.net:27017,ac-kxcl5ym-shard-00-01.s7g6for.mongodb.net:27017,ac-kxcl5ym-shard-00-02.s7g6for.mongodb.net:27017/?ssl=true&replicaSet=atlas-mgt8y5-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const MONGO_URI = `mongodb://bhagataman1999:AmanKumar@ac-mvrqgwo-shard-00-00.yl38szv.mongodb.net:27017,ac-mvrqgwo-shard-00-01.yl38szv.mongodb.net:27017,ac-mvrqgwo-shard-00-02.yl38szv.mongodb.net:27017/?ssl=true&replicaSet=atlas-zszke0-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
         await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
         console.log('Database connected successfully');
